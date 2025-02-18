53b2ff9f324f753424cbae5e4c1185d9643bc259
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-02-18 00:14:14 +0000

    hero section

--- App/src/components/Navbar.vue
+++ App/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <nav class="bg-transparent border-gray-200 dark:bg-transparent dark:border-gray-700 relative z-20">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ayoub Tribak</span>
@@ -16,7 +16,7 @@
             </button>
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul
-                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
 
                     <li>
                         <router-link to="/"
