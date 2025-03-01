3f4327dde0c1c635056c833660c83f7e1f8e38e7
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-03-01 14:02:09 +0000

    update navbar

--- App/src/components/Navbar.vue
+++ App/src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
-    <nav class="bg-transparent border-gray-200 dark:bg-transparent dark:border-gray-700 relative z-20"
+    <nav class="shadow-sm bg-white border-gray-200 dark:bg-white dark:border-gray-700 fixed top-0 left-0 right-0 w-full z-20"
         aria-label="Main navigation">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 fixed top-0 right-0 left-0">
+        <div class="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4">
             <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Ayoub Tribak</span>
+                <span class="self-center text-lg font-semibold whitespace-nowrap text-black">Ayoub Tribak</span>
             </a>
             <button @click="toggleMenu" type="button"
-                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300"
                 aria-controls="navbar-default" :aria-expanded="isMenuOpen">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -15,31 +15,30 @@
                         d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
-            <div :class="[
-                isMenuOpen ? 'block bg-white dark:bg-gray-800' : 'hidden',
-                'w-full md:block md:w-auto transition-colors duration-300'
-            ]" id="navbar-default">
+            <div :class="[isMenuOpen ? 'block bg-white' : 'hidden', 'w-full md:block md:w-auto transition-colors duration-300 bg-white']"
+                id="navbar-default">
                 <ul
-                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
+                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-white md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
                     <li>
-                        <router-link to="/" class="nav-link" :class="{ 'active-link': activeSection === 'home' }"
+                        <router-link to="/" class="nav-link text-black"
+                            :class="{ 'active-link': activeSection === 'home' }"
                             @click="scrollToSection('home')">Home</router-link>
                     </li>
                     <li>
-                        <button @click="scrollToSection('about')" class="nav-link"
+                        <button @click="scrollToSection('about')" class="nav-link text-black"
                             :class="{ 'active-link': activeSection === 'about' }">About</button>
                     </li>
                     <li>
-                        <button @click="scrollToSection('projects')" class="nav-link"
+                        <button @click="scrollToSection('projects')" class="nav-link text-black"
                             :class="{ 'active-link': activeSection === 'projects' }">Projects</button>
                     </li>
                     <li>
-                        <router-link to="/resume" class="nav-link"
+                        <router-link to="/resume" class="nav-link text-black"
                             :class="{ 'active-link': activeSection === '/resume' }"
                             @click="scrollToSection('/resume')">Resume</router-link>
                     </li>
                     <li>
-                        <button @click="scrollToSection('contact')" class="nav-link"
+                        <button @click="scrollToSection('contact')" class="nav-link text-black"
                             :class="{ 'active-link': activeSection === 'contact' }">Contact</button>
                     </li>
                 </ul>
