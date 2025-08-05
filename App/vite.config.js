38f19c7615032483cbd8dd5f6a6f13eef027f4d4
douaebh <douaaebh18@gmail.com>
2025-08-05 18:59:48 +0100

    fix to redeploy

--- App/vite.config.js
+++ App/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    include: ['lucide-vue-next'] // نجبر Vite باش يجهز هاد المكتبة مسبقًا
+  },
+  build: {
+    rollupOptions: {
+      external: [] // نحيد أي تجاهل خاطئ
+    },
+    commonjsOptions: {
+      include: [/node_modules/, 'lucide-vue-next'] // باش يسمح بترجمتها فـ build
+    }
+  }
 })
